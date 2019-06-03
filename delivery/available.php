caa35418edcf188b1ffa93dffbe8a81ac7c4a3c9
Shaheer <officialshaheer@gmail.com>
2019-06-04 04:02:37 +0530

    Create available.php

---
+++ delivery/available.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+
+        <meta charset="UTF-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <meta name="theme-color" content="#881c58">
+
+        <link rel="icon" href="img/footer/fav_icon1.png" type="image/jpg" sizes="16x16">
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <style type="text/css">
+        
+    body {
+      background-color: #466020;
+      color: white;
+      margin: 0;
+      }
+      .container {
+      position: fixed;
+      background-color:#881058;
+      border-radius: 12px;
+      height: 100%;
+      width:100%;
+      color: white;
+      top: 0%;
+    }  
+    .header {
+      position: relative;
+      background-color:#881058;
+      border-radius: 12px;
+      height: auto;
+      width:50%;
+      color: white;
+      top: 0%;
+      left: 25%;
+    }  
+    .sub {
+      background-color: #FCFEFB;
+      border-radius: 12px;
+      height: 100%;
+      width:100%;
+      color: #333;
+      position: absolute;
+    }
+   .sub-availableworkers {
+      background-color: #333;
+      height: 100%;
+      width:15%;
+      color: #466020;
+      position: relative;
+   }
+   input[type=button], input[type=submit], input[type=reset] {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+       </style>    
+
+
+    </head>
+           
+<body>
+  <div class="container">
+
+  <div class="header">
+  <center><h1>Delivery System - Doc-Care Live</h1></center>
+  </div>
+
+  <div class="sub">
+  <center><h2>Workers Available Now</h2></center>  
+
+  <div class="sub-availableworkers">
+   <center>
+  <input type="button"  name="delBtn" value="View Available Workers"></center> 
+
+  </div>
+
+</div>
+
+</body>   
+</html>
+
